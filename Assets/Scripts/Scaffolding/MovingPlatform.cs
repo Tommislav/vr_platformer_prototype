235daf9fb444b2d33302707345fcb9ef3bd8fcb7
Tommislav <tommy@salomonsson.se>
2016-02-26 23:07:30 +0100

    started on a scaffolding level, high high up

---
+++ Assets/Scripts/Scaffolding/MovingPlatform.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+
+	public float time = 2f;
+	public float pause = 2f;
+
+	private Vector3 p1;
+	private Vector3 p2;
+	private int count;
+
+	void Start() {
+		p1 = transform.FindChild("p1").position;
+		p2 = transform.FindChild("p2").position;
+
+		Move();
+	}
+
+	private void Move() {
+		count++;
+		Vector3 target = count % 2 == 0 ? p1 : p2;
+		LeanTween.move(gameObject, target, time).setDelay(pause).setOnComplete(Move);
+	}
+
+	void OnDestroy() {
+		LeanTween.cancel(gameObject);
+	}
+}
---
+++ Assets/Scripts/Scaffolding/Spin90Deg.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin90Deg : MonoBehaviour {
+
+	private float rot = 0f;
+
+
+	void Start () {
+		rot = transform.rotation.eulerAngles.y;
+		OnRotDone();
+	}
+
+	private void OnRotDone() {
+		rot += 90f;
+		rot %= 360;
+		LeanTween.rotate(gameObject, new Vector3(0, rot, 0), 2f).setDelay(2f).setOnComplete(OnRotDone);
+
+	}
+
+
+	void OnDestroy() {
+		LeanTween.cancel(gameObject);
+	}
+
+}
